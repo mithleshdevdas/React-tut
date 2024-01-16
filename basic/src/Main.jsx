bea1aa4484a719863eb0333b5bee87274ff86ed8
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-16 13:11:45 +0530

    cumstom react

--- basic/src/Main.jsx
+++ basic/src/Main.jsx
@@ -3,9 +3,24 @@ import React from 'react'
 import ReactDOM  from 'react-dom/client'
 import App from './App.jsx'
 
+function Say() {
+ return <h1>wowowowowo</h1>
+}
+
+const Another = (
+  <a href="https://google.com" target='_blank'>google</a>
+)
+const Reactele = React.createElement(
+  'h1',
+  {},
+  'hello mithlesh'
+ )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  // Another
+   Reactele
+  // <Say/>
+
+
 )
